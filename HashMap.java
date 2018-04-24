0a4d65dd98f01803867d24f239bd4cbe647d4fe8
Niraj Sazzie <mr.nirajjee@gmail.com>
2018-04-24 22:59:49 +0530

    Create HashMap.java

---
+++ HashMap.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class HashMap{
+
+     public static void main(String []args){
+         Map<Integer,String> m = new HashMap<Integer,String>();
+         
+         System.out.println(m.put(101,"Hello"));
+         System.out.println(m.put(102,"Mikki"));
+         System.out.println(m.put(101,"Nikki"));
+         System.out.println(m.put(103,"Sazzie"));
+         
+         // m.putAll
+         System.out.println("--------------------------");
+         System.out.println(m.get(101));
+         System.out.println(m.get(102));
+         
+         //m.remove(key) will remove the entry
+         System.out.println(m.remove(103));
+         
+         System.out.println("---------------------------");
+         
+         System.out.println(m.containsKey(101));
+         System.out.println(m.containsKey(103));
+         
+         System.out.println("---------------------------");
+         
+         System.out.println(m.containsValue("Hello"));
+         System.out.println(m.containsValue("Mikki"));
+         
+         System.out.println("---------------------------");
+         
+         System.out.println(m.isEmpty());
+         System.out.println("---------------------------");
+         System.out.println(m.size());
+     }
+}
